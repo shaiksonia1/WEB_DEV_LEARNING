d4cd657bf7fc39e2b6cc1a8d7d1afac90cd2cb09
Shaik Sonia <shaiksonia222@gmail.com>
2024-09-27 02:36:49 +0530

    datatypes

---
+++ JS_learning/data_types.js
@@ -0,0 +1,19 @@
+// variables are represented by var 
+// data types
+var name = "string"
+var number= 23
+var boolean = true
+var boolean = false
+
+// to print we use console.log
+console.log(name)
+
+// we should always use camelCase while delaring variables 
+
+// concatenation
+var firstName = "Shaik"
+var lastName = "Sonia"
+console.log("Hello " +firstName + lastName)
+
+// slicing
+firstName.slice(0,3)
