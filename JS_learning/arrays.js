44d2b85ce12bd17d2c90aec40bbc65bb1c56b497
Shaik Sonia <shaiksonia222@gmail.com>
2024-09-27 02:36:41 +0530

    arrays

---
+++ JS_learning/arrays.js
@@ -0,0 +1,27 @@
+var output = [];
+var count =1;
+
+function fizzBuzz(){
+    
+    while(count<=100){
+    if(count%3===0 && count%5===0){
+        output.push("fizzBuzz")
+    }
+    else if (count%3 ===0 ){
+        output.push("Fizz")
+
+    }
+    else if(count%5===0 ){
+        output.push("Buzz")
+
+    }
+    else{
+        output.push(count);
+
+    }
+        count++;
+console.log(output)
+}
+}
+
+fizzBuzz();
