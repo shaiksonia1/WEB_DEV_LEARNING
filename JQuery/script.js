49863ebea52d85386388566fee9cbd2f151a3252
Shaik Sonia <shaiksonia222@gmail.com>
2024-10-03 20:35:04 +0530

    JQuery

--- JQuery/script.js
+++ JQuery/script.js
@@ -1,2 +1,17 @@
 // document.getElementsByTagName("h1")[0].style.color = "pink";
-$("h1").css('color',"red")
+// $("h1").addClass("big-title")
+$("button").css("backgroundColor","pink")
+
+
+$("h1").click(function(){
+    $("h1").addClass("big-title")
+    
+
+})
+$("button").click(function(){
+    $("h1").css("color","purple");
+});
+$(document).keypress(function(Event){
+    $("h1").text(Event.key)
+
+})
